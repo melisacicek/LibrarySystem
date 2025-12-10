b0663ebec4c1e2824c15f9a1516a8530ea98e162
Melisa Çiçek <melissaciceksoyubey@gmail.com>
2025-12-10 11:49:07 +0300

    Refactor Main to launch JavaFX application
    Replaces manual database and admin logic in Main with a call to LibraryApp.main(args) to start the JavaFX application. This streamlines the entry point and prepares the project for a GUI-based workflow.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,6 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
-
-        KullanıcıVeriTabaniOluştur kullanıcıVeriTabaniOluştur = new KullanıcıVeriTabaniOluştur();
-        KitapVeriTabaniOluştur kitapVeriTabaniOluştur = new KitapVeriTabaniOluştur();
-        AdminVeriTabaniOluştur adminVeriTabaniOluştur = new AdminVeriTabaniOluştur();
-
-
-        System.out.println("\nTüm kullanıcılar:");
-        kullanıcıVeriTabaniOluştur.tumKullanicilar().forEach(u ->
-                System.out.println(u.getKullaniciİd() + " - " + u.getAdSoyad())
-        );
-
-        System.out.println("\nTüm kitaplar:");
-        kitapVeriTabaniOluştur.tumKitaplar().forEach(b ->
-                System.out.println(b.getKitapİd() + " - " + b.getKitapAdi())
-        );
-
-
-        if (adminVeriTabaniOluştur.adminGiris("Melisa", "1234")) {
-            System.out.println("Admin girişi başarılı!");
-        } else {
-            System.out.println("Admin bilgileri hatalı!");
-        }
+        // JavaFX uygulamasını başlat
+        LibraryApp.main(args);
     }
 }
